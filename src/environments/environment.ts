0d6dab13df1a4427e3bca7ae559e4af2238107b1
AdrianSilvaNascimento <adrianfsf10@gmail.com>
2023-09-14 18:11:29 -0300

    fix URL variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  URL: 'http://localhost:3004',
+  URL: 'https://pet-id-server.vercel.app',
   mapUrl: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
   mapAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   integrator_api_key: '33ea269d-7f92-4c76-8a0e-ebf09a8177ba',
